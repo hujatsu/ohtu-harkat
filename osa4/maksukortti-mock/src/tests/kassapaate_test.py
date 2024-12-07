fe9c2fb9bd7dd18af964d15323185f7ade0bd43e
Niko Hujanen <hujatsu@gmail.com>
2024-12-07 18:48:39 +0200

    Kassapäätteen lataa metodin testit

--- osa4/maksukortti-mock/src/tests/kassapaate_test.py
+++ osa4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -11,7 +11,7 @@ class TestKassapaate(unittest.TestCase):
     def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
         maksukortti_mock = Mock()
         maksukortti_mock.saldo = 10
-        
+
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_called_with(HINTA)
@@ -19,7 +19,37 @@ class TestKassapaate(unittest.TestCase):
     def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
         maksukortti_mock = Mock()
         maksukortti_mock.saldo = 4
-        
+
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortti_ladataan_positiivisella_summalla(self):
+        """Testaa, että maksukortin latausmetodia kutsutaan positiivisella
+        summalla."""
+        maksukortti_mock = Mock()
+        SUMMA = 1
+
+        self.kassa.lataa(maksukortti_mock, SUMMA)
+
+        maksukortti_mock.lataa.assert_called_with(SUMMA)
+
+    def test_korttia_ei_ladata_nolla_summalla(self):
+        """Tehtävä ei spesifioinut, ladataanko korttia nollasummalla vai ei.
+        Päätin, ettei ladata."""
+        maksukortti_mock = Mock()
+        SUMMA = 0
+
+        self.kassa.lataa(maksukortti_mock, SUMMA)
+
+        maksukortti_mock.lataa.assert_not_called()
+
+    def test_korttia_ei_ladata_negatiivisella_summalla(self):
+        """Testaa, ettei maksukortin latausmetodia kutsuta lainkaan, jos summa
+        on negativiinen."""
+        maksukortti_mock = Mock()
+        SUMMA = -1
+
+        self.kassa.lataa(maksukortti_mock, SUMMA)
+
+        maksukortti_mock.lataa.assert_not_called()
